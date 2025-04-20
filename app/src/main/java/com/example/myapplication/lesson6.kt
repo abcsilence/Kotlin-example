d29160385ab44ba58eb4e1f64f9646b44313e0c3
User <admin@admins-MacBook-Air.local>
2025-04-20 23:34:39 +0545

    Operator

---
+++ app/src/main/java/com/example/myapplication/lesson6.kt
@@ -0,0 +1,33 @@
+package com.example.myapplication
+
+//Arithmetic operator.
+fun main() {
+    val a = 10
+    val b = 5
+
+    println("a + b = ${a + b}")
+    println("a - b = ${a - b}")
+    println("a * b = ${a * b}")
+    println("a / b = ${a / b}")
+    println("a % b = ${a % b}")
+}
+//Comparision Operator.
+//fun main() {
+//    val x = 10
+//    val y = 5
+//
+//    println(x > y)
+//    println(x < y)
+//    println(x != y)
+//    println(x >= y)
+//    println(x <= y)
+//}
+//Logical Operator.
+//fun main() {
+//    val isSunny = true
+//    val isWeekend = false
+//
+//    println(isSunny && isWeekend)
+//    println(isSunny || isWeekend)
+//    println(!isSunny)
+//}

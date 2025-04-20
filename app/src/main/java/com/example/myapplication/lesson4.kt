240c65f89ff0266b4db7b0f4c7e7a1de8c8235b4
User <admin@admins-MacBook-Air.local>
2025-04-20 23:24:40 +0545

    Array and arraylist

---
+++ app/src/main/java/com/example/myapplication/lesson4.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication
+
+
+//Array example
+fun main() {
+    // Array with 3 items
+    var numbers = arrayOf(1, 2, 3)
+
+    // Print first item (index 0)
+    println(numbers[0])  // Output: 1
+}
+
+//Array list example
+//fun main() {
+//    // Create an empty ArrayList
+//    var names = arrayListOf<String>()
+//
+//    // Add items
+//    names.add("Roshan")
+//    names.add("Dipak")
+//
+//    list
+//    fun main() {
+//        val fruits = listOf("Apple", "Banana", "Apple")
+//        println(fruits[0])  // Output: Apple
+//    }

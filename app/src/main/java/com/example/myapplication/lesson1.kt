9bec66ae2c2abec003c8ec8d28a8ff85befd3dc7
User <admin@admins-MacBook-Air.local>
2025-04-20 23:09:08 +0545

    Variable example

--- app/src/main/java/com/example/myapplication/lesson1.kt
+++ app/src/main/java/com/example/myapplication/lesson1.kt
@@ -1,13 +1,49 @@
 package com.example.myapplication
 
 //Variable example.
+//fun main() {
+ //   var name = "Roshan"   // This value can change
+ //   val age = 20          // This value cannot change
+
+ //   println("My name is $name")
+ //   println("I am $age years old")
+
+ //   name = "Roshan K"
+ //   println("Now my name is $name")
+//}
+//dataype example
+// fun main() {
+//
+//     var name: String = "Roshan"
+//
+//
+//     var age: Int = 20
+//
+//
+//     var isStudent: Boolean = true
+//
+//
+//     println("Name: $name")
+//     println("Age: $age")
+//     println("Is student: $isStudent")
+// }
+
+//Type conversion example
+//String to int
+//fun main() {
+//    val str = "10"
+//    val num = str.toInt()
+//    println(num + 5)
+//}
+
+//int to string
 fun main() {
-    var name = "Roshan"   // This value can change
-    val age = 20          // This value cannot change
-
-    println("My name is $name")
-    println("I am $age years old")
-
-    name = "Roshan K"
-    println("Now my name is $name")
+    val num = 25
+    val str = num.toString()
+    println("My number is $str")  // Output: My number is 25
 }
+
+
+
+
+

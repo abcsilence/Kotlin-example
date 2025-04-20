555441e72fa52a64408be3727217ef461bfcfac3
User <admin@admins-MacBook-Air.local>
2025-04-20 23:42:41 +0545

    condtional statement

---
+++ app/src/main/java/com/example/myapplication/lesson7.kt
@@ -0,0 +1,47 @@
+package com.example.myapplication
+//
+//}
+////if statement
+//fun main() {
+//    val age = 20
+//    if (age >= 18) {
+//        println("You are an adult")
+//    }
+//if else statement
+//fun main() {
+//    val number = 5
+//    if (number % 2 == 0) {
+//        println("Even number")
+//    } else {
+//        println("Odd number")
+//    }
+//}
+
+//for loop
+
+//fun main() {
+//    for (i in 1..5) {
+//        println("For loop: $i")
+//    }
+//}
+
+
+//while loop
+//fun main() {
+//    var i = 1
+//    while (i <= 3) {
+//        println("While loop: $i")
+//        i++
+//    }
+//}
+
+
+//dowhile
+//fun main() {
+//    var i = 1
+//    do {
+//        println("Do-While loop: $i")
+//        i++
+//    } while (i <= 2)
+//}
+

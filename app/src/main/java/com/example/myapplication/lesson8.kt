7bde082361835bb630a59a50497ccd57fcbe4a31
User <admin@admins-MacBook-Air.local>
2025-04-20 23:46:07 +0545

    Function

---
+++ app/src/main/java/com/example/myapplication/lesson8.kt
@@ -0,0 +1,37 @@
+package com.example.myapplication
+
+//Function.
+//fun main() {
+//    sayHello()
+//    addNumbers(5, 3)
+//}
+//
+//
+//fun sayHello() {
+//    println("Hello, Roshan!")
+//}
+
+//
+//fun addNumbers(a: Int, b: Int) {
+//    val sum = a + b
+//    println("Sum is: $sum")
+//}
+
+//fun greet() {
+//    println("Hello, Roshan")
+//}
+//
+//fun main() {
+//    greet()
+//}
+
+fun add(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun main() {
+    val result = add(3, 4)
+    println(result)
+}
+
+

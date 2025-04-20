266e82298c5ef84be124769396fd9e8dd6628cc9
User <admin@admins-MacBook-Air.local>
2025-04-20 22:40:51 +0545

    Vaiable example

---
+++ app/src/main/java/com/example/myapplication/lesson1.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+
+//Variable example.
+fun main() {
+    var name = "Roshan"   // This value can change
+    val age = 20          // This value cannot change
+
+    println("My name is $name")
+    println("I am $age years old")
+
+    name = "Roshan K"
+    println("Now my name is $name")
+}
